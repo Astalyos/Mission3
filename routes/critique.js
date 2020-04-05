935200eb66a01770a1dbf2d9613627c98e1dcb75
Balgor18 <catinaud.florian@gmail.com>
2020-04-05 22:49:15 +0200

    Correction de BUG sur les commentaires

--- routes/critique.js
+++ routes/critique.js
@@ -7,19 +7,19 @@ var axios = require('axios');
 router.get('/', async function (req, res, next) {
     var db = req.db;
     var collectFilmUserCommented = await db.get('films').find();
-    // console.log(collectFilmUserCommented);
+    var getPseudo = req.session.pseudo;
     var getUid = req.session.uid;
     var data = "";
     var getGenre = await axios.get('https://api.themoviedb.org/3/genre/movie/list?api_key=67c91e6c478de75dad308e127da768bf&language=fr');
     var toutLesGenres = getGenre.data.genres;
-    await Film.find({"commentaires.uid": getUid},{"commentaires.commentaire":1,"commentaires.note":1,"title":1,"poster_path":1,"overview":1,"release_date":1,"genre":1,"idFilm":1},
+    await Film.find({"commentaires.pseudo":getPseudo,"commentaires.uid":getUid},{"commentaires.commentaire":1,"commentaires.note":1,"commentaires.pseudo":1,"title":1,"poster_path":1,"overview":1,"release_date":1,"genre":1,"idFilm":1},
     async function (err, result) {
         if(err){
             console.log(err);
         }
         data = result
     })
-    console.log(collectFilmUserCommented)
+    console.log(data)
 
     // console.log(collectFilmUserCommented[0].commentaires)
     res.render('critique', {
